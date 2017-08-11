35e5bc760f42ae5c72ab67d63f6309791e170a61
yangqihua <904693433@qq.com>
2017-08-12 01:15:47 +0800

    去掉了一些header中一些菜单

--- application/extra/site.php
+++ application/extra/site.php
@@ -24,9 +24,9 @@ return array (
   array (
     'basic' => '基础配置',
     'email' => '邮件配置',
-    'dictionary' => '字典配置',
-    'user' => '会员配置',
-    'example' => '示例分组',
+//    'dictionary' => '字典配置',
+//    'user' => '会员配置',
+//    'example' => '示例分组',
   ),
   'mail_type' => '1',
   'mail_smtp_host' => 'smtp.exmail.qq.com',
