efdab990df9d8c58dd6d0a3dcf034c2ded8ef3be
yangqihua <904693433@qq.com>
2017-09-25 23:40:38 +0800

    可视化完成部分

--- application/admin/controller/Dashboard.php
+++ application/admin/controller/Dashboard.php
@@ -4,6 +4,8 @@ namespace app\admin\controller;
 
 use app\common\controller\Backend;
 
+use think\Db;
+
 /**
  * 控制台
  *
@@ -26,19 +28,30 @@ class Dashboard extends Backend
             $createlist[$day] = mt_rand(20, 200);
             $paylist[$day] = mt_rand(1, mt_rand(1, $createlist[$day]));
         }
+
+
+        $totalOrderItem = Db::query('SELECT seller_sku sku,count(*) num FROM fastadmin.order_item group by seller_sku');
+        $bgClass = ['st-green','st-green','st-green','st-green','st-violet','st-violet','st-violet','st-violet','st-blue','st-blue','st-blue','st-blue'];
+        for ($i = 0; $i < count($totalOrderItem); $i++){
+            $totalOrderItem[$i]['bgClass'] = $bgClass[$i];
+            $totalOrderItem[$i]['sku'] = preg_replace('/HOLSEM[-| ]/','',$totalOrderItem[$i]['sku']);
+        }
+
+        $orderItems = Db::query('SELECT from_unixtime(create_time,\'%Y年%m月%d日\') createTime,seller_sku,count(*) num from fastadmin.order_item 
+group by createTime,seller_sku;');
+        $ctTimeItems = [];
+        for ($i = 0; $i < count($orderItems); $i++){
+            $item = $orderItems[$i];
+            if(array_key_exists($item['createTime'],$ctTimeItems)){
+                $ctTimeItems[$item['createTime']][$item['seller_sku']] = $item['num'];
+            }else{
+                $ctTimeItems[$item['createTime']] = [];
+                $ctTimeItems[$item['createTime']][$item['seller_sku']] = $item['num'];
+            }
+        }
+
         $this->view->assign([
-            'totaluser'          => 35200,
-            'totalviews'         => 219390,
-            'totalorder'         => 32143,
-            'totalorderamount'   => 174800,
-            'todayuserlogin'     => 321,
-            'todayusersignup'    => 430,
-            'todayorder'         => 2324,
-            'todayunsettleorder' => 132,
-            'sevendnu'           => '80%',
-            'sevendau'           => '32%',
-            'paylist'            => $paylist,
-            'createlist'         => $createlist,
+            'totalOrderItem' => $totalOrderItem,
         ]);
 
 
--- public/assets/js/backend/dashboard.js
+++ public/assets/js/backend/dashboard.js
@@ -3,107 +3,214 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'table', 'echarts', 'echart
     var Controller = {
         index: function () {
             // 基于准备好的dom，初始化echarts实例
-            var myChart = Echarts.init(document.getElementById('echart'), 'walden');
+            var myChart = Echarts.init(document.getElementById('echart'));
+
+            var xData = function () {
+                var data = [];
+                for (var i = 1; i < 13; i++) {
+                    data.push(i + "月份");
+                }
+                return data;
+            }();
 
             // 指定图表的配置项和数据
             var option = {
-                title: {
-                    text: '',
-                    subtext: ''
+                "title": {
+                    text: '订单详情',
+                    textStyle: {
+                        color: '#27C24C',
+                        fontSize: '16'
+                    },
                 },
-                tooltip: {
-                    trigger: 'axis'
-                },
-                legend: {
-                    data: ['下单', '成交']
+                "tooltip": {
+                    "trigger": "axis",
+                    // "axisPointer": {
+                    //     "type": "shadow",
+                    //     textStyle: {
+                    //         color: "#fff"
+                    //     }
+                    // },
                 },
                 toolbox: {
-                    show: false,
+                    right: 16,
                     feature: {
-                        magicType: {show: true, type: ['stack', 'tiled']},
-                        saveAsImage: {show: true}
+                        saveAsImage: {}
                     }
                 },
-                xAxis: {
-                    type: 'category',
-                    boundaryGap: false,
-                    data: Orderdata.column
+                "grid": {
+                    // "borderWidth": 0,
+                    // "top": 110,
+                    "bottom": 100,
+                    // textStyle: {
+                    //     color: "#fff"
+                    // }
                 },
-                yAxis: {
+                "legend": {
+                    // x: '4%',
+                    // top: '11%',
+                    // textStyle: {
+                    //     color: '#90979c',
+                    // },
+                    "data": ['OA']
+                },
 
-                },
-                grid: [{
-                        left: 'left',
-                        top: 'top',
-                        right: '10',
-                        bottom: 30
-                    }],
-                series: [{
-                        name: '成交',
-                        type: 'line',
-                        smooth: true,
-                        areaStyle: {
-                            normal: {
-                            }
+                // "calculable": true,
+                "xAxis": [{
+                    "type": "category",
+                    // "axisLine": {
+                    //     lineStyle: {
+                    //         color: '#90979c'
+                    //     }
+                    // },
+                    "splitLine": {
+                        "show": false
+                    },
+                    "axisTick": {
+                        "show": false
+                    },
+                    "splitArea": {
+                        "show": false
+                    },
+                    // "axisLabel": {
+                    //     "interval": 0,
+                    // },
+                    "data": xData,
+                }],
+                "yAxis": [{
+                    "type": "value",
+                    "splitLine": {
+                        "show": false
+                    },
+                    // "axisLine": {
+                    //     lineStyle: {
+                    //         color: '#90979c'
+                    //     }
+                    // },
+                    "axisTick": {
+                        "show": false
+                    },
+                    "axisLabel": {
+                        "interval": 0,
+
+                    },
+                    "splitArea": {
+                        "show": false
+                    },
+
+                }],
+                "dataZoom": [
+                    {
+                        // "show": true,
+                        // "height": 30,
+                        "xAxisIndex": [
+                            0
+                        ],
+                        bottom: 30,
+                        "start": 0,
+                        "end": 100,
+                        handleIcon: 'path://M306.1,413c0,2.2-1.8,4-4,4h-59.8c-2.2,0-4-1.8-4-4V200.8c0-2.2,1.8-4,4-4h59.8c2.2,0,4,1.8,4,4V413z',
+                        handleSize: '110%',
+                        handleStyle: {
+                            color: "#aaa",
+
                         },
-                        lineStyle: {
-                            normal: {
-                                width: 1.5
-                            }
+                        textStyle: {
+                            color: "#27C24C"
                         },
-                        data: Orderdata.paydata
+                        borderColor: "#aaa"
+
+
                     },
                     {
-                        name: '下单',
-                        type: 'line',
-                        smooth: true,
-                        areaStyle: {
-                            normal: {
-                            }
-                        },
-                        lineStyle: {
-                            normal: {
-                                width: 1.5
-                            }
-                        },
-                        data: Orderdata.createdata
-                    }]
+                        "type": "inside",
+                        "show": true,
+                        // "height": 15,
+                        "start": 30,
+                        "end": 100
+                    }
+                ],
+                "series": [
+                    {
+                        "name": "OA",
+                        "type": "bar",
+                        "stack": "总量",
+                        "barMaxWidth": 40,
+                        "data": [
+                            709,
+                            1917,
+                            2455,
+                            2610,
+                            1719,
+                            1433,
+                            1544,
+                            3285,
+                            5208,
+                            3372,
+                            2484,
+                            4078
+                        ],
+                    },
+
+                    {
+                        "name": "手工录入",
+                        "type": "bar",
+                        "stack": "总量",
+                        "data": [
+                            327,
+                            1776,
+                            507,
+                            1200,
+                            800,
+                            482,
+                            204,
+                            1390,
+                            1001,
+                            951,
+                            381,
+                            220
+                        ]
+                    },
+
+                    {
+                        "name": "企业号",
+                        "type": "bar",
+                        "stack": "总量",
+                        "data": [
+                            327,
+                            1776,
+                            507,
+                            1200,
+                            800,
+                            482,
+                            204,
+                            1390,
+                            1001,
+                            951,
+                            381,
+                            220
+                        ]
+                    }, {
+                        "name": "总数",
+                        "type": "line",
+                        "data": [
+                            1363,
+                            3693,
+                            2962,
+                            3810,
+                            2519,
+                            1915,
+                            1748,
+                            4675,
+                            6209,
+                            4323,
+                            2865,
+                            4298
+                        ]
+                    },
+                ]
             };
 
-            // 使用刚指定的配置项和数据显示图表。
             myChart.setOption(option);
-
-            //动态添加数据，可以通过Ajax获取数据然后填充
-            setInterval(function () {
-                Orderdata.column.push((new Date()).toLocaleTimeString().replace(/^\D*/, ''));
-                var amount = Math.floor(Math.random() * 200) + 20;
-                Orderdata.createdata.push(amount);
-                Orderdata.paydata.push(Math.floor(Math.random() * amount) + 1);
-
-                //按自己需求可以取消这个限制
-                if (Orderdata.column.length >= 20) {
-                    //移除最开始的一条数据
-                    Orderdata.column.shift();
-                    Orderdata.paydata.shift();
-                    Orderdata.createdata.shift();
-                }
-                myChart.setOption({
-                    xAxis: {
-                        data: Orderdata.column
-                    },
-                    series: [{
-                            name: '成交',
-                            data: Orderdata.paydata
-                        },
-                        {
-                            name: '下单',
-                            data: Orderdata.createdata
-                        }]
-                });
-            }, 2000);
-            $(window).resize(function () {
-                myChart.resize();
-            });
         }
     };
 
