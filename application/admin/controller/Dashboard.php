16bbe816f39f8890e9d7940c3d97757c99a3a166
yangqihua <904693433@qq.com>
2017-09-26 12:29:31 +0800

    订单数据可视化

--- application/admin/controller/Dashboard.php
+++ application/admin/controller/Dashboard.php
@@ -22,8 +22,7 @@ class Dashboard extends Backend
     {
         $seventtime = \fast\Date::unixtime('day', -7);
         $paylist = $createlist = [];
-        for ($i = 0; $i < 7; $i++)
-        {
+        for ($i = 0; $i < 7; $i++) {
             $day = date("Y-m-d", $seventtime + ($i * 86400));
             $createlist[$day] = mt_rand(20, 200);
             $paylist[$day] = mt_rand(1, mt_rand(1, $createlist[$day]));
@@ -31,27 +30,69 @@ class Dashboard extends Backend
 
 
         $totalOrderItem = Db::query('SELECT seller_sku sku,count(*) num FROM fastadmin.order_item group by seller_sku');
-        $bgClass = ['st-green','st-green','st-green','st-green','st-violet','st-violet','st-violet','st-violet','st-blue','st-blue','st-blue','st-blue'];
-        for ($i = 0; $i < count($totalOrderItem); $i++){
+        $bgClass = ['st-green', 'st-green', 'st-green', 'st-green', 'st-violet', 'st-violet', 'st-violet', 'st-violet', 'st-blue', 'st-blue', 'st-blue', 'st-blue'];
+        for ($i = 0; $i < count($totalOrderItem); $i++) {
             $totalOrderItem[$i]['bgClass'] = $bgClass[$i];
-            $totalOrderItem[$i]['sku'] = preg_replace('/HOLSEM[-| ]/','',$totalOrderItem[$i]['sku']);
+            $totalOrderItem[$i]['sku'] = preg_replace('/HOLSEM[-| ]/', '', $totalOrderItem[$i]['sku']);
         }
 
-        $orderItems = Db::query('SELECT from_unixtime(create_time,\'%Y年%m月%d日\') createTime,seller_sku,count(*) num from fastadmin.order_item 
-group by createTime,seller_sku;');
+        $firstDay = strtotime("-1 month");
+        $sql = 'SELECT from_unixtime(create_time,\'%m月%d日\') createTime,seller_sku,count(*) num from fastadmin.order_item 
+WHERE create_time>:firstDay group by createTime,seller_sku;';
+        $orderItems = Db::query($sql,['firstDay'=>$firstDay]);
         $ctTimeItems = [];
-        for ($i = 0; $i < count($orderItems); $i++){
+        for ($i = 0; $i < count($orderItems); $i++) {
             $item = $orderItems[$i];
-            if(array_key_exists($item['createTime'],$ctTimeItems)){
-                $ctTimeItems[$item['createTime']][$item['seller_sku']] = $item['num'];
-            }else{
+            if (array_key_exists($item['createTime'], $ctTimeItems)) {
+                $ctTimeItems[$item['createTime']][preg_replace('/HOLSEM[-| ]/', '', $item['seller_sku'])] = $item['num'];
+            } else {
                 $ctTimeItems[$item['createTime']] = [];
-                $ctTimeItems[$item['createTime']][$item['seller_sku']] = $item['num'];
+                $ctTimeItems[$item['createTime']][preg_replace('/HOLSEM[-| ]/', '', $item['seller_sku'])] = $item['num'];
+//                $ctTimeItems[$item['createTime']]['createTime'] = $item['createTime'];
+            }
+        }
+        $allKeys = [];
+        foreach ($ctTimeItems as $value) {
+            foreach ($value as $k=>$v) {
+                if(!(in_array($k,$allKeys))){
+                    $allKeys[] = $k;
+                }
+            }
+        }
+        sort($allKeys);
+
+        foreach ($ctTimeItems as $k=>$value) {
+            foreach ($allKeys as $key) {
+                if(!array_key_exists($key,$value)){
+                    $ctTimeItems[$k][$key] = 0;
+                }
+            }
+            ksort($ctTimeItems[$k]);
+        }
+
+        $resultList = [];
+        foreach ($ctTimeItems as $value) {
+            $resultList[] = array_values($value);
+        }
+        $result = [array()];
+        for ($i = 0; $i < count($resultList); $i++) {
+            for ($j = 0; $j < count($allKeys); $j++) {
+                if (array_key_exists($i, $resultList) && array_key_exists($j, $resultList[$i])) {
+                    $result[$j][$i] = $resultList[$i][$j];
+                } else {
+                    $result[$j][$i] = 0;
+                }
             }
         }
 
+        $date = array_keys($ctTimeItems);
+
         $this->view->assign([
             'totalOrderItem' => $totalOrderItem,
+            'date' => $date,
+            'keys' => $allKeys,
+            'data' => $result,
+
         ]);
 
 
--- public/assets/js/backend/dashboard.js
+++ public/assets/js/backend/dashboard.js
@@ -6,11 +6,37 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'table', 'echarts', 'echart
             var myChart = Echarts.init(document.getElementById('echart'));
 
             var xData = function () {
-                var data = [];
-                for (var i = 1; i < 13; i++) {
-                    data.push(i + "月份");
+                var date = orderItems.date;
+                var keys = orderItems.keys;
+                var data = orderItems.data;
+                var series = [];
+
+                for (var i = 0; i < keys.length; i++) {
+                    var item = {};
+                    item['name'] = keys[i];
+                    item['type'] = 'bar';
+                    item['stack'] = 'orderItem';
+                    item['barMaxWidth'] = '40';
+                    item['data'] = data[i];
+                    series.push(item);
                 }
-                return data;
+                var total = [];
+                for (var i = 0; i < data.length; i++) {
+                    for (var j = 0; j < data[i].length; j++) {
+                        if (!total.hasOwnProperty(j)) {
+                            total[j] = 0;
+                        }
+                        total[j] += data[i][j];
+                        if(j===1){
+                        // console.log("total["+j+"]: ", total[j], "data["+i+"]["+j+"]", data[i][j]);
+                        }
+                    }
+                }
+                var lineItem = {name: '总数', type: 'line', data: total};
+                series.push(lineItem);
+                console.log("date:", date);
+                // console.log("data:", data);
+                return {date: date, keys: keys, series: series};
             }();
 
             // 指定图表的配置项和数据
@@ -51,7 +77,7 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'table', 'echarts', 'echart
                     // textStyle: {
                     //     color: '#90979c',
                     // },
-                    "data": ['OA']
+                    "data": xData.keys,
                 },
 
                 // "calculable": true,
@@ -74,7 +100,7 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'table', 'echarts', 'echart
                     // "axisLabel": {
                     //     "interval": 0,
                     // },
-                    "data": xData,
+                    "data": xData.date,
                 }],
                 "yAxis": [{
                     "type": "value",
@@ -129,85 +155,86 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'table', 'echarts', 'echart
                         "end": 100
                     }
                 ],
-                "series": [
-                    {
-                        "name": "OA",
-                        "type": "bar",
-                        "stack": "总量",
-                        "barMaxWidth": 40,
-                        "data": [
-                            709,
-                            1917,
-                            2455,
-                            2610,
-                            1719,
-                            1433,
-                            1544,
-                            3285,
-                            5208,
-                            3372,
-                            2484,
-                            4078
-                        ],
-                    },
-
-                    {
-                        "name": "手工录入",
-                        "type": "bar",
-                        "stack": "总量",
-                        "data": [
-                            327,
-                            1776,
-                            507,
-                            1200,
-                            800,
-                            482,
-                            204,
-                            1390,
-                            1001,
-                            951,
-                            381,
-                            220
-                        ]
-                    },
-
-                    {
-                        "name": "企业号",
-                        "type": "bar",
-                        "stack": "总量",
-                        "data": [
-                            327,
-                            1776,
-                            507,
-                            1200,
-                            800,
-                            482,
-                            204,
-                            1390,
-                            1001,
-                            951,
-                            381,
-                            220
-                        ]
-                    }, {
-                        "name": "总数",
-                        "type": "line",
-                        "data": [
-                            1363,
-                            3693,
-                            2962,
-                            3810,
-                            2519,
-                            1915,
-                            1748,
-                            4675,
-                            6209,
-                            4323,
-                            2865,
-                            4298
-                        ]
-                    },
-                ]
+                "series": xData.series,
+                //     [
+                //     {
+                //         "name": "OA",
+                //         "type": "bar",
+                //         "stack": "总量",
+                //         "barMaxWidth": 40,
+                //         "data": [
+                //             709,
+                //             1917,
+                //             2455,
+                //             2610,
+                //             1719,
+                //             1433,
+                //             1544,
+                //             3285,
+                //             5208,
+                //             3372,
+                //             2484,
+                //             4078
+                //         ],
+                //     },
+                //
+                //     {
+                //         "name": "手工录入",
+                //         "type": "bar",
+                //         "stack": "总量",
+                //         "data": [
+                //             327,
+                //             1776,
+                //             507,
+                //             1200,
+                //             800,
+                //             482,
+                //             204,
+                //             1390,
+                //             1001,
+                //             951,
+                //             381,
+                //             220
+                //         ]
+                //     },
+                //
+                //     {
+                //         "name": "企业号",
+                //         "type": "bar",
+                //         "stack": "总量",
+                //         "data": [
+                //             327,
+                //             1776,
+                //             507,
+                //             1200,
+                //             800,
+                //             482,
+                //             204,
+                //             1390,
+                //             1001,
+                //             951,
+                //             381,
+                //             220
+                //         ]
+                //     }, {
+                //         "name": "总数",
+                //         "type": "line",
+                //         "data": [
+                //             1363,
+                //             3693,
+                //             2962,
+                //             3810,
+                //             2519,
+                //             1915,
+                //             1748,
+                //             4675,
+                //             6209,
+                //             4323,
+                //             2865,
+                //             4298
+                //         ]
+                //     },
+                // ]
             };
 
             myChart.setOption(option);
