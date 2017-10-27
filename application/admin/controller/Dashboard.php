ceb165092b3586b08c388135f2ade8421bf167fd
yangqihua <904693433@qq.com>
2017-10-27 18:43:12 +0800

    初步完成

--- application/admin/controller/Dashboard.php
+++ application/admin/controller/Dashboard.php
@@ -3,8 +3,10 @@
 namespace app\admin\controller;
 
 use app\common\controller\Backend;
-
+use app\admin\model\Config as ConfigModel;
+use app\admin\model\Inventory as InventoryModel;
 use think\Db;
+
 
 /**
  * 控制台
@@ -20,26 +22,10 @@ class Dashboard extends Backend
      */
     public function index()
     {
-        $seventtime = \fast\Date::unixtime('day', -7);
-        $paylist = $createlist = [];
-        for ($i = 0; $i < 7; $i++) {
-            $day = date("Y-m-d", $seventtime + ($i * 86400));
-            $createlist[$day] = mt_rand(20, 200);
-            $paylist[$day] = mt_rand(1, mt_rand(1, $createlist[$day]));
-        }
-
-
-        $totalOrderItem = Db::query('SELECT seller_sku sku,count(*) num FROM fastadmin.order_item group by seller_sku');
-        $bgClass = ['st-green', 'st-green', 'st-green', 'st-green', 'st-violet', 'st-violet', 'st-violet', 'st-violet', 'st-blue', 'st-blue', 'st-blue', 'st-blue'];
-        for ($i = 0; $i < count($totalOrderItem); $i++) {
-            $totalOrderItem[$i]['bgClass'] = $bgClass[$i];
-            $totalOrderItem[$i]['sku'] = preg_replace('/HOLSEM[-| ]/', '', $totalOrderItem[$i]['sku']);
-        }
-
-        $firstDay = strtotime(date('Y-m-d',strtotime("-2 month")));
+        $firstDay = strtotime(date('Y-m-d', strtotime("-2 month")));
         $sql = 'SELECT from_unixtime(create_time,\'%m月%d日\') createTime,seller_sku,count(*) num from fastadmin.order_item 
 WHERE create_time>:firstDay group by createTime,seller_sku;';
-        $orderItems = Db::query($sql,['firstDay'=>$firstDay]);
+        $orderItems = Db::query($sql, ['firstDay' => $firstDay]);
         $ctTimeItems = [];
         for ($i = 0; $i < count($orderItems); $i++) {
             $item = $orderItems[$i];
@@ -53,23 +39,21 @@ WHERE create_time>:firstDay group by createTime,seller_sku;';
         }
         $allKeys = [];
         foreach ($ctTimeItems as $value) {
-            foreach ($value as $k=>$v) {
-                if(!(in_array($k,$allKeys))){
+            foreach ($value as $k => $v) {
+                if (!(in_array($k, $allKeys))) {
                     $allKeys[] = $k;
                 }
             }
         }
         sort($allKeys);
-
-        foreach ($ctTimeItems as $k=>$value) {
+        foreach ($ctTimeItems as $k => $value) {
             foreach ($allKeys as $key) {
-                if(!array_key_exists($key,$value)){
+                if (!array_key_exists($key, $value)) {
                     $ctTimeItems[$k][$key] = 0;
                 }
             }
             ksort($ctTimeItems[$k]);
         }
-
         $resultList = [];
         foreach ($ctTimeItems as $value) {
             $resultList[] = array_values($value);
@@ -84,17 +68,96 @@ WHERE create_time>:firstDay group by createTime,seller_sku;';
                 }
             }
         }
-
         $date = array_keys($ctTimeItems);
 
+        // 1.首页的当前库存量
+        $sql = 'SELECT * from inventory where id=(select max(id) from inventory);';
+        $latestInventories = Db::query($sql);
+        $latestInventory = $latestInventories[0];
+
+        $configModel = new ConfigModel();
+        $configs = $configModel->where("group", "inventory")->select();
+
+        // 1.首页的当前库存量
+        $latestInventory7 = $latestInventory;
+        $sql = 'SELECT * from inventory order by id limit 7;';
+        $latestInventories7 = Db::query($sql);
+        if (count($latestInventories7) == 7) {
+            $latestInventory7 = $latestInventories7[6];
+        }
+
+        $inventoryDataList = [];
+        foreach ($latestInventory as $key => $latestValue) {
+            if ($key == 'id' || $key == 'createtime' || $key == 'updatetime') {
+                continue;
+            }
+
+            $inventoryData['average'] = ($latestValue - $latestInventory7[$key]) / 7;
+
+            $inventoryData['sku'] = $key;
+            $inventoryData['inventory'] = $latestValue;
+
+            foreach ($configs as $k => $config) {
+                $name = $config['name'];
+                $configValue = $config['value'];
+                $cs = explode("_", $name);
+                $c_sku = $cs[0];
+                $kind = $cs[1];
+                if ($c_sku == $key) {
+                    if ($kind == 'inventory') {
+                        $inventoryData['security_inventory'] = $configValue;
+                    } else {
+                        $inventoryData['inventory_duration'] = $configValue;
+                    }
+                    if (array_key_exists('security_inventory', $inventoryData)
+                        && array_key_exists('inventory_duration', $inventoryData)
+                    ) {
+                        break;
+                    }
+                }
+            }
+            $p = $inventoryData['average'] * $inventoryData['inventory_duration'] + $inventoryData['security_inventory'];
+            $statusClass = 'success';
+            $status = '安全';
+            $remark = '当前库存' . $latestValue . ' > ' . $p . '=(' . $inventoryData['average'] . '*' . $inventoryData['inventory_duration'] . '+' . $inventoryData['security_inventory'].')';
+            if ($latestValue < $p && $latestValue > $inventoryData['security_inventory']) {
+                $status = '紧张';
+                $statusClass = 'warning';
+                $remark = '安全库存' . $inventoryData['security_inventory'] . ' < 当前库存' . $latestValue . ' < ' . $p . '=(' . $inventoryData['average'] . '*' . $inventoryData['inventory_duration'] . '+' . $inventoryData['security_inventory'].')';
+            } else if ($latestValue < $inventoryData['security_inventory']) {
+                $status = '危险';
+                $statusClass = 'danger';
+                $remark = '当前库存' . $latestValue . ' < 安全库存' . $inventoryData['security_inventory'];
+            }
+            $inventoryData['status'] = $status;
+            $inventoryData['remark'] = $remark;
+            $inventoryData['statusClass'] = $statusClass;
+            $inventoryDataList[] = $inventoryData;
+        }
+
+        // 2.首页的库存折线图
+        $inventoryModel = new InventoryModel();
+        $inventoryChartDataListDb = $inventoryModel->select();
+        $inventoryChartDataList = [];
+        foreach ($inventoryChartDataListDb as $key=>$value){
+            $inventoryChartDataList['time'][] = datetime($value['createtime'],'Y-m-d');
+            $inventoryChartDataList['s12'][] = $value['s12'];
+            $inventoryChartDataList['x12b'][] = $value['x12b'];
+            $inventoryChartDataList['x8b'][] = $value['x8b'];
+            $inventoryChartDataList['x8'][] = $value['x8'];
+            $inventoryChartDataList['x5b'][] = $value['x5b'];
+            $inventoryChartDataList['x5'][] = $value['x5'];
+            $inventoryChartDataList['a1'][] = $value['a1'];
+        }
+
         $this->view->assign([
-            'totalOrderItem' => $totalOrderItem,
             'date' => $date,
             'keys' => $allKeys,
             'data' => $result,
 
+            'inventoryDataList' => $inventoryDataList,
+            'inventoryChartDataList'=>$inventoryChartDataList,
         ]);
-
 
         return $this->view->fetch();
     }
--- public/assets/js/backend/dashboard.js
+++ public/assets/js/backend/dashboard.js
@@ -3,9 +3,8 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'table', 'echarts', 'echart
     var Controller = {
         index: function () {
             // 基于准备好的dom，初始化echarts实例
-            var myChart = Echarts.init(document.getElementById('echart'));
-
-            var xData = function () {
+            var unitsChart = Echarts.init(document.getElementById('units_echart'));
+            var unitsData = function () {
                 var date = orderItems.date;
                 var keys = orderItems.keys;
                 var data = orderItems.data;
@@ -38,9 +37,8 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'table', 'echarts', 'echart
                 // console.log("data:", data);
                 return {date: date, keys: keys, series: series};
             }();
-
             // 指定图表的配置项和数据
-            var option = {
+            var unitsOption = {
                 "title": {
                     text: '销量详情',
                     textStyle: {
@@ -61,7 +59,7 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'table', 'echarts', 'echart
                     "bottom": 100,
                 },
                 "legend": {
-                    "data": xData.keys,
+                    "data": unitsData.keys,
                 },
 
                 // "calculable": true,
@@ -76,7 +74,7 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'table', 'echarts', 'echart
                     "splitArea": {
                         "show": false
                     },
-                    "data": xData.date,
+                    "data": unitsData.date,
                 }],
                 "yAxis": [{
                     "type": "value",
@@ -121,10 +119,107 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'table', 'echarts', 'echart
                         "end": 100
                     }
                 ],
-                "series": xData.series
+                "series": unitsData.series
             };
+            unitsChart.setOption(unitsOption);
 
-            myChart.setOption(option);
+            var inventoryChart = Echarts.init(document.getElementById('inventory_echart'));
+            var inventoryOption = {
+                title: {
+                    text: '库存统计',
+                    textStyle: {
+                        color: '#27C24C',
+                        fontSize: '16'
+                    },
+                },
+                tooltip: {
+                    trigger: 'axis'
+                },
+                legend: {
+                    data:['s12','x12','x8b','x8','x5b','x5','a1']
+                },
+                grid: {
+                    "bottom": 100,
+                },
+                toolbox: {
+                    feature: {
+                        saveAsImage: {}
+                    }
+                },
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    data: inventoryChartDataList['time']
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                dataZoom: [
+                    {
+                        "xAxisIndex": [
+                            0
+                        ],
+                        bottom: 30,
+                        "start": 0,
+                        "end": 100,
+                        handleIcon: 'path://M306.1,413c0,2.2-1.8,4-4,4h-59.8c-2.2,0-4-1.8-4-4V200.8c0-2.2,1.8-4,4-4h59.8c2.2,0,4,1.8,4,4V413z',
+                        handleSize: '110%',
+                        handleStyle: {
+                            color: "#aaa",
+                        },
+                        textStyle: {
+                            color: "#27C24C"
+                        },
+                        borderColor: "#aaa"
+                    },
+                    {
+                        "type": "inside",
+                        "show": true,
+                        // "height": 15,
+                        "start": 30,
+                        "end": 100
+                    }
+                ],
+                series: [
+                    {
+                        name:'s12',
+                        type:'line',
+                        data:inventoryChartDataList['s12']
+                    },
+                    {
+                        name:'x12b',
+                        type:'line',
+                        data:inventoryChartDataList['x12b']
+                    },
+                    {
+                        name:'x8b',
+                        type:'line',
+                        data:inventoryChartDataList['x8b']
+                    },
+                    {
+                        name:'x8',
+                        type:'line',
+                        data:inventoryChartDataList['x8']
+                    },
+                    {
+                        name:'x5b',
+                        type:'line',
+                        data:inventoryChartDataList['x5b']
+                    },
+                    {
+                        name:'x5',
+                        type:'line',
+                        data:inventoryChartDataList['x5']
+                    },
+                    {
+                        name:'a1',
+                        type:'line',
+                        data:inventoryChartDataList['a1']
+                    }
+                ]
+            };
+            inventoryChart.setOption(inventoryOption);
+
         }
     };
 
