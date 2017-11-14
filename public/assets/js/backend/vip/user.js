81fefcb4ef47e3a5f0686cc03a7f85eb76eacca4
yangqihua <904693433@qq.com>
2017-11-14 17:07:19 +0800

    加上vip的一些额外信息

--- public/assets/js/backend/vip/user.js
+++ public/assets/js/backend/vip/user.js
@@ -24,19 +24,22 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                 columns: [
                     [
                         {checkbox: true},
-                        {field: 'id', title: __('Id')},
+                        // {field: 'id', title: __('Id')},
                         {field: 'order_id', title: __('Order_id')},
+                        {field: 'skus', title: '购买的sku'},
                         {field: 'firstname', title: __('Firstname')},
+                        {field: 'lastname', title: __('Lastname')},
                         {field: 'email', title: __('Email')},
                         {field: 'telephone', title: __('Telephone')},
                         {field: 'fax', title: __('Fax')},
-                        {field: 'lastname', title: __('Lastname')},
-                        {field: 'address_1', title: __('Address_1')},
-                        {field: 'address_2', title: __('Address_2')},
+                        {field: 'address_1', title: __('Address_1'),visible: false,},
+                        {field: 'address_2', title: __('Address_2'),visible: false,},
                         {field: 'city', title: __('City')},
-                        {field: 'postcode', title: __('Postcode')},
+                        {field: 'postcode', title: __('Postcode'),visible: false,},
                         {field: 'company', title: __('Company')},
-                        {field: 'updatetime', title: __('Updatetime'), formatter: Table.api.formatter.datetime},
+                        {field: 'mail_msg', title: '邮件发送内容',visible: false},
+                        {field: 'remark', title: '备注',visible: false},
+                        {field: 'updatetime', title: __('Updatetime'), formatter: Table.api.formatter.datetime,visible: false,},
                         {field: 'createtime', title: __('Createtime'), formatter: Table.api.formatter.datetime},
                         {field: 'operate', title: __('Operate'), events: Table.api.events.operate, formatter: Table.api.formatter.operate}
                     ]
