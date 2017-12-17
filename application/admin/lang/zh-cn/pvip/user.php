fce94c596e6cd2ac68d8a3f32f23682a8605dd04
yangqihua <904693433@qq.com>
2017-12-18 00:52:54 +0800

    生成代码

---
+++ application/admin/lang/zh-cn/pvip/user.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'Register_date'  =>  'Register date',
+    'Name'  =>  'Name',
+    'Email'  =>  'E-mail',
+    'Tel'  =>  'TEL',
+    'Product'  =>  'Product',
+    'Asin'  =>  'ASIN',
+    'Order_id'  =>  'Order ID',
+    'Order_date'  =>  'Order Date',
+    'Warranty'  =>  'Warranty Extension',
+    'Remark1'  =>  '备注1',
+    'Remark2'  =>  '备注2'
+];
---
+++ public/assets/js/backend/pvip/user.js
@@ -0,0 +1,59 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'pvip/user/index',
+                    add_url: 'pvip/user/add',
+                    edit_url: 'pvip/user/edit',
+                    del_url: 'pvip/user/del',
+                    multi_url: 'pvip/user/multi',
+                    table: 'pvip_user',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'order_id',
+                sortName: 'order_id',
+                columns: [
+                    [
+                                                {field: 'register_date', title: __('Register_date')},
+                        {field: 'name', title: __('Name')},
+                        {field: 'email', title: __('Email')},
+                        {field: 'tel', title: __('Tel')},
+                        {field: 'product', title: __('Product')},
+                        {field: 'asin', title: __('Asin')},
+{checkbox: true},
+                        {field: 'order_id', title: __('Order_id')},
+                        {field: 'order_date', title: __('Order_date')},
+                        {field: 'Warranty', title: __('Warranty')},
+                        {field: 'remark1', title: __('Remark1')},
+                        {field: 'remark2', title: __('Remark2')},
+                        {field: 'operate', title: __('Operate'), events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
