8d9f70596d8c23d84ee67b28d80283332d26682e
yangqihua <904693433@qq.com>
2018-03-21 22:30:26 +0800

    没有邮箱的邮件不发送

--- application/api/controller/amazon/Order.php
+++ application/api/controller/amazon/Order.php
@@ -312,6 +312,7 @@ class Order extends Api
         $order_packages = "USPS,UPS";
         $beginDate = '2018-03-01 00:00:00';
         $order_other_count = $this->orderModel
+            ->where("buyer_email", "<>", null)
             ->where("has_send_mail", "<>", "1")
             ->where("purchase_date", ">", $beginDate)
             ->where("purchase_date", "<", $twoWeeksAgo)
@@ -327,6 +328,7 @@ class Order extends Api
         }
 
         $orders = $this->orderModel
+            ->where("buyer_email", "<>", null)
             ->where("has_send_mail", "<>", "1")
             ->where("purchase_date", ">", $beginDate)
             ->where("purchase_date", "<", $twoWeeksAgo)
